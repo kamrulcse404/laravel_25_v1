231615e23f0a2c92df22b0da9285cb19c0b109e6
kamrulcse404 <kamrul.h01960@gmail.com>
2025-01-26 14:00:54 +0600

    query builder part 2

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Route;
 
 use function Laravel\Prompts\select;
@@ -27,7 +28,28 @@ Route::get('/users', function () {
 
     // $users = DB::table('users')->get();
     // $users = DB::table('users')->find(10);
-    $users = DB::table('users')->select('name', 'email')->find(10);
+    // $users = DB::table('users')->select('name', 'email')->find(10);
+
+
+
+    // DB::table('users')->insert([
+    //     ['name' => 'Farjana', 'email' => 'farjana1@gmail.com', 'password' => Hash::make('password')],
+    //     ['name' => 'Farjana1', 'email' => 'farjana2@gmail.com', 'password' => Hash::make('password')],
+    //     ['name' => 'Farjana2', 'email' => 'farjana3@gmail.com', 'password' => Hash::make('password')],
+    //     ['name' => 'Farjana2', 'email' => 'farjana4@gmail.com', 'password' => Hash::make('password')],
+    // ]);
+
+    // $users = DB::table('users')->select('name', 'email', 'country')->orderBy('id', 'desc')->first();
+
+
+    // $users = DB::table('users')->where('is_admin', '=', true)->get();
+    // $users = DB::table('users')->where('is_admin', true)->get();
+
+    // $users = DB::table('users')->where('country', 'Bangladesh')->update(['country' => 'Canada']);
+    // $users = DB::table('users')->where('country', 'Canada')->get();
+
+    // DB::table('users')->where('country', 'Kenya')->delete();
+    $users = DB::table('users')->get();
 
 
     return $users;
